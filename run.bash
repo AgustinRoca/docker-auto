797ff9c93fb178f05acd3f3df74ce517b16539a2
Agustín Roca <aroca@udesa.edu.ar>
2024-03-18 16:09:45 -0300

    Change helper roslaunch

--- run.bash
+++ run.bash
@@ -1,4 +1,4 @@
-echo roslaunch turtlebot_gazebo turtlebot_world.launch
+echo roslaunch dataspeed_dbw_gazebo dataspeed_dbw_gazebo.launch sim_param_file:=/auto/src/simulation/yaml/sim_params.yaml use_camera_control:=false
 echo 
 
 rocker --volume $(pwd)/default.rviz:/opt/ros/noetic/share/rviz/default.rviz /home/aroca/Documents/auto:/auto --x11 ros1-noetic-auto
